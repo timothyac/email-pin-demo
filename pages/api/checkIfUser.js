c930be04a4829fdcdcc2e39ffb2a077590b1f704
Timothy Caron <aus10caron@icloud.com>
2020-12-28 05:21:03 -0500

    adding some realism

--- pages/api/checkIfUser.js
+++ pages/api/checkIfUser.js
@@ -1,4 +1,8 @@
 export default (req, res) => {
   res.statusCode = 200;
-  res.json({ verified: true });
+
+  // Simulate a network delay
+  setTimeout(() => {
+    res.json({ verified: true });
+  }, 1000);
 };
